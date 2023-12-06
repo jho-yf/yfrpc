dee85491a0f8078ab2574e763fc56708e8e9425c
jho <xujihong@bingosoft.net>
2023-12-06 09:44:08 +0800

    feat: 扫描jar包中指定包名下的所有类信息

--- yfrpc-common/src/main/java/cn/jho/yfrpc/common/scanner/ClassScanner.java
+++ yfrpc-common/src/main/java/cn/jho/yfrpc/common/scanner/ClassScanner.java
@@ -1,7 +1,13 @@
 package cn.jho.yfrpc.common.scanner;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * 通用类扫描器
@@ -45,6 +51,10 @@ public class ClassScanner {
         // 获取子目录和以.class结尾的文件
         File[] files = dir.listFiles(
                 file -> (recursive && file.isDirectory()) || file.getName().endsWith(CLASS_FILE_SUFFIX));
+        if (files == null) {
+            return;
+        }
+
         for (File file : files) {
             if (file.isDirectory()) {
                 // 递归扫描子目录
@@ -57,6 +67,52 @@ public class ClassScanner {
             }
         }
 
+    }
+
+    /**
+     * 扫描jar包中指定包名下的所有类信息
+     *
+     * @param pkgName    扫描的包名
+     * @param url        Jar包的url路径
+     * @param pkgDirName 当前包名所属包名
+     * @param recursive  是否递归调用
+     * @param classNames 类名列表
+     * @throws IOException 读取jar包失败
+     */
+    private static String findAndAddClassesInPkgByJar(String pkgName, URL url, String pkgDirName,
+            final boolean recursive,
+            List<String> classNames) throws IOException {
+        JarFile jar = ((JarURLConnection) url.openConnection()).getJarFile();
+        Enumeration<JarEntry> entries = jar.entries();
+        while (entries.hasMoreElements()) {
+            JarEntry entry = entries.nextElement();
+            String name = entry.getName();
+
+            // 如果以"/"开头，则去掉开头的"/"
+            if (name.charAt(0) == '/') {
+                name = name.substring(1);
+            }
+
+            // 不是在指定包名下，跳过
+            if (!name.startsWith(pkgDirName)) {
+                continue;
+            }
+
+            int idx = name.lastIndexOf('/');
+
+            // 如果以"/"为结尾，则是一个包路径
+            if (idx != -1) {
+                pkgName = name.substring(0, idx).replace("/", ".");
+            }
+
+            if ((idx != -1 || recursive) && (name.endsWith(CLASS_FILE_SUFFIX) && !entry.isDirectory())) {
+                // 去掉class文件的后缀 ".class"
+                String className = name.substring(pkgName.length() + 1, name.length() - 6);
+                classNames.add(className);
+            }
+        }
+
+        return pkgName;
     }
 
 }
