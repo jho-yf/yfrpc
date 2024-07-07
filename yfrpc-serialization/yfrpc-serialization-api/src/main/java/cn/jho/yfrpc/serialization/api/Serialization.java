b0365215c5393304f21c3972d97ae1e7c08e6936
jho <xujihong@bingosoft.net>
2024-07-07 11:28:20 +0800

    docs(yfrpc-serialization): 补充javadocs

--- yfrpc-serialization/yfrpc-serialization-api/src/main/java/cn/jho/yfrpc/serialization/api/Serialization.java
+++ yfrpc-serialization/yfrpc-serialization-api/src/main/java/cn/jho/yfrpc/serialization/api/Serialization.java
@@ -11,11 +11,22 @@ public interface Serialization<E> {
 
     /**
      * 序列化
+     *
+     * @param <T> 序列化对象类型
+     * @param obj 序列化对象
+     * @return 序列化结果
+     * @throws SerializerException 序列化异常
      */
     <T extends E> byte[] serialize(T obj) throws SerializerException;
 
     /**
      * 反序列化
+     *
+     * @param <T>  反序列化对象类型
+     * @param data 需要反序列化的数据
+     * @param cls  反序列化目标 Class 对象
+     * @return 反序列化结果
+     * @throws SerializerException 反序列化异常
      */
     <T extends E> T deserialize(byte[] data, Class<T> cls) throws SerializerException;
 
